06be4c5fe44bfb7a210986b13e8f558e726d9d06
Abderrahim <aitbali@hotmail.com>
2019-07-07 20:36:35 +0200

    create persona model

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_07_07_183325) do
+
+  create_table "personas", force: :cascade do |t|
+    t.string "name"
+    t.integer "pv"
+    t.integer "pa"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
